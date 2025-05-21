21167b1e3ce9a148947ff53b6c6e457ccfa866a8
shubhampawale7 <shubhampawale7@gmail.com>
2025-05-21 15:51:34 +0530

    feat: Update Navbar branding from 'Portfolio' to 'Shubham Pawale'

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,7 +35,7 @@ export const Navbar = () => {
           href="#hero"
         >
           <span className="relative z-10">
-            <span className="text-glow text-foreground"> </span> Portfolio
+            <span className="text-glow text-foreground"> </span> Shubham Pawale
           </span>
         </a>
 
