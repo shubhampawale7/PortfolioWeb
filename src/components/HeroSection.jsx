e9acb6c1c4dfdaf6c45dd3387d22cec49d4c693d
shubhampawale7 <shubhampawale7@gmail.com>
2025-05-22 19:37:02 +0530

    feat: Adjust spacing and visibility in HeroSection for improved layout and user experience

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -74,7 +74,7 @@ export const HeroSection = () => {
             visually compelling.
           </p>
 
-          <div className="pt-2 opacity-0 animate-fade-in-delay-4">
+          <div className="mt-8 opacity-0 animate-fade-in-delay-4">
             <a href="#projects" className="cosmic-button">
               View My Work
             </a>
@@ -82,10 +82,10 @@ export const HeroSection = () => {
         </div>
       </motion.div>
 
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce ">
+      {/* <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce ">
         <span className="text-sm text-muted-foreground "> Scroll </span>
         <ArrowDown className="h-5 w-5 text-primary" />
-      </div>
+      </div> */}
     </section>
   );
 };
